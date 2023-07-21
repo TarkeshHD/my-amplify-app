58ad15c01d97fd68c0bc984cca95b07d27344d79
Kaushal Panchal <krdpanchal@gmail.com>
2023-07-21 12:10:17 +0530

    Side nav nested items active logic

--- src/layouts/SideNavBar/SideNavNestedItems.jsx
+++ src/layouts/SideNavBar/SideNavNestedItems.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Collapse, List } from '@mui/material';
 import { SideNavItem } from './SideNavItem';
 
-export const SideNavNestedItems = ({ active, item, ...props }) => {
+export const SideNavNestedItems = ({ item, ...props }) => {
   const [open, setOpen] = useState(false);
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (item?.children?.find((v) => v.path === pathname)) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }, [pathname, item.children]);
   return (
     <>
       <SideNavItem
-        active={open}
         disabled={item.disabled}
         external={item.external}
         icon={open ? <ExpandLess /> : <ExpandMore />}
@@ -24,7 +32,7 @@ export const SideNavNestedItems = ({ active, item, ...props }) => {
           {(item.children || []).map((item) => (
             <SideNavItem
               sx={{ pl: 4 }}
-              active={active}
+              active={item.path ? pathname === item.path : false}
               disabled={item.disabled}
               external={item.external}
               icon={item.icon}
